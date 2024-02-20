645616e82daeb44de72d0f454bca3a20217a5714
ayush tyagi <ayushtyagi4810poco@gmail.com>
2024-02-20 21:35:58 +0530

    done

---
+++ app/(nav-items)/blog/[slug]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignlePostPage = () => {
+    return (
+        <div>SignlePostPage</div>
+    )
+}
+
+export default SignlePostPage
--- app/(nav-items)/blog/page.jsx
+++ app/(nav-items)/blog/page.jsx
@@ -1,36 +1,24 @@
 import PostCard from '@/app/components/postCard/PostCard';
-import React from 'react'
+import { getPosts } from '@/lib/data';
 import styles from "./blog.module.css"
 
-const BlogPage = () => {
+const BlogPage = async () => {
+
+    // FETCH DATA WITH AN API
+    const posts = await getPosts();
+
+    // FETCH DATA WITHOUT AN API
+    // const posts = await getPosts();
+
     return (
-        <div className={styles.container} >
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div> <div className={styles.post}>
-                <PostCard />
-            </div> <div className={styles.post}>
-                <PostCard />
-            </div> <div className={styles.post}>
-                <PostCard />
-            </div> <div className={styles.post}>
-                <PostCard />
-            </div> <div className={styles.post}>
-                <PostCard />
-            </div>
-
-
+        <div className={styles.container}>
+            {posts.map((post) => (
+                <div className={styles.post} key={post.id}>
+                    <PostCard post={post} />
+                </div>
+            ))}
         </div>
-    )
-}
+    );
+};
 
 export default BlogPage;
